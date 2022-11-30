ab7f683860251b2ede83df694ebaa8defe13f1c9
Maxen24 <mshubham242@gmail.com>
2022-11-30 15:47:27 +0530

    Added UC-2-Day46Problem

--- assets-20221114T041410Z-001/EmployeePayroll.js
+++ assets-20221114T041410Z-001/EmployeePayroll.js
@@ -9,3 +9,38 @@ const remove =(node)=>{
     document.querySelector(".emp-count").textContent=empPayrollList.length
     createInnerHtml()
 }
+/// uc2
+const checkForUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson ? true : false;
+     if (!isUpdate) return;
+     employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm();
+}
+    const setForm=() => {    
+    setValue('#name', employeePayrollobj._name);    
+    setSelectedValues('[name=profile]', employeePayrollobj._profilePic); 
+    setSelectedValues('[name=gender]', employeePayrollobj._gender);
+    setSelectedValues('[name=department]', employeePayrollobj._department); 
+    setValue('#salary', employeePayrollobj._salary); 
+    setTextValue('.salary-output', employeePayrollobj._salary);
+    setValue('#notes', employeePayrollobj._note);
+    let date= stringifyDate(employeePayroll0bj._startDate).split(" "); 
+    setValue('#day, date[0]');
+    setValue('month', date[1]);
+    setValue('#year', date[2]);
+    }
+    const setSelectedValues=(propertyValue,value)=>{    
+        let allItems= document.querySelectorAll(propertyValue);
+        allItems.forEach(item => {    
+            if(Array.isArray(value)) {
+                 if (value.includes(item.value)) {
+                    item.checked= true;
+                 }
+                }
+        
+            else if (item.value === value){
+             item.checked= true;
+            }
+        })
+    }
